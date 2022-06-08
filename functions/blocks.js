15f6ace56b53be902baeae6f34142fc777981667
Siddharth Vadgama <me@siddv.net>
2022-06-08 15:54:06 +0100

    feat(api): add get blocks endpoint

---
+++ functions/blocks.js
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const base = require('airtable').base(process.env.AIRTABLE_BASE);
+
+exports.handler = async (event, context) => {
+  try {
+    const response = await base.table(process.env.AIRTABLE_TABLE).select().all()
+
+    const blockList = []
+    response.forEach(item => {
+      blockList.push(item.fields)
+    })
+
+    return {
+      headers: {
+        "content-type": "application/json"
+      },
+      statusCode: 200,
+      body: JSON.stringify({
+        blockList,
+      })
+    }
+  } catch (e) {
+    console.log('error', e)
+
+    return {
+      headers: {
+        "content-type": "application/json"
+      },
+      statusCode: 500,
+      body: JSON.stringify({
+        oof: "oof",
+      })
+    }
+  }
+}
